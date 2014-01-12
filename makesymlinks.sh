52d3c6049b0cde172b94bc31c2240ccae568e249
tavizon <iktome@adohi.net>
2014-01-11 20:24:01 -0800

    simplified makesymlinks.sh and edited the output

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 #
-# makesymlinks.sh
+# makesymlinks.sh - Jesse Tavizon - iktome@adohi.net
 # symlinks for .[file|directory] in ~/ to matching git sources in ~/dotfiles
 
 # Variables
 dir=~/dotfiles                   # dotfiles directory
-olddir=~/dotfiles_old            # original dotfiles backup directory
+olddir=$dir/dotfiles_old            # original dotfiles backup directory
 files="vimrc"                    # list of files/folders to symlink
 
-echo -n "Creating $olddir for backup of any existing dotfiles in ~ ..."
-mkdir -p $olddir
-echo "done"
-
-echo -n "Changing to the $dir directory ..."
+echo -n "Changing to the $dir directory"
 cd $dir
 echo "done"
 
+echo -n "Creating $olddir for backup of any existing dotfiles in ~/"
+mkdir -p $olddir
+echo "done"
+
 for file in $files; do
-    echo "Moving any existing dotfiles from ~ to $olddir"
-    mv ~/.$file ~/dotfiles_old/
+    echo "Backing up existing .$file in ~/ if needed"
+    mv ~/.$file $olddir 
     echo "Creating symlink to $file."
     ln -s $dir/$file ~/.$file
 done
