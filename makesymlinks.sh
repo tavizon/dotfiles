bd0e9f89f644373aa9e151b6b0a000fb51d0465a
tavizon <iktome@adohi.net>
2014-01-11 20:03:28 -0800

    edited makesymlinks.sh for more personal style and legibility

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -1,32 +1,24 @@
 #!/bin/bash
 #
 # makesymlinks.sh
-#
-# This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
+# symlinks for .[file|directory] in ~/ to matching git sources in ~/dotfiles
 
-#
 # Variables
-#
-dir=~/dotfiles                    # dotfiles directory
-olddir=~/dotfiles_old             # old dotfiles backup directory
-files="vimrc"    # list of files/folders to symlink in homedir
+dir=~/dotfiles                   # dotfiles directory
+olddir=~/dotfiles_old            # original dotfiles backup directory
+files="vimrc"                    # list of files/folders to symlink
 
-#
-# create dotfiles_old in homedir
-#
 echo -n "Creating $olddir for backup of any existing dotfiles in ~ ..."
 mkdir -p $olddir
 echo "done"
 
-# change to the dotfiles directory
 echo -n "Changing to the $dir directory ..."
 cd $dir
 echo "done"
 
-# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
 for file in $files; do
     echo "Moving any existing dotfiles from ~ to $olddir"
     mv ~/.$file ~/dotfiles_old/
-    echo "Creating symlink to $file in home directory."
+    echo "Creating symlink to $file."
     ln -s $dir/$file ~/.$file
 done
